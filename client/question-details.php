df3e0c6f5e842f503dc31d0f4f7310e865946a33
Navjosan44 <navdeep3170930@gmail.com>
2026-09-22 16:59:28 +0530

    just one more page its a answer page create a new file for showing answers

--- client/question-details.php
+++ client/question-details.php
@@ -10,11 +10,12 @@
         $row = $result->fetch_assoc();
         echo "<h4 class='margin-bottom-15 question-title' >Question : " . $row['title'] . "</h4>
     <p class='margin-bottom-15'>" . $row['description'] . "</p>";
+        include("answers.php")
         ?>
         <form action="./server/requests.php" method="post">
             <input type="hidden" name="question_id" value="<?php echo $qid; ?>">
-        <textarea name="answer" class="form-control margin-bottom-15" placeholder="Write your answer..."></textarea>
-        <button class="btn btn-primary">Submit</button>
+            <textarea name="answer" class="form-control margin-bottom-15" placeholder="Write your answer..."></textarea>
+            <button class="btn btn-primary">Submit</button>
         </form>
     </div>
 </div>
